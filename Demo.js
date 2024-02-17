c07aadf625c2a80bf2191e51e8bae178cfb6f380
Alireza Nazari <alireza.ginbox@gmail.com>
2024-02-17 15:29:30 +0430

    init repo

---
+++ Demo.js
@@ -0,0 +1,154 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 127.0.0.1 [Mongo]
+ Source Server Type    : MongoDB
+ Source Server Version : 70005 (7.0.5)
+ Source Host           : localhost:27017
+ Source Schema         : Demo
+
+ Target Server Type    : MongoDB
+ Target Server Version : 70005 (7.0.5)
+ File Encoding         : 65001
+
+ Date: 17/02/2024 14:34:45
+*/
+
+
+// ----------------------------
+// Collection structure for DemoCollection
+// ----------------------------
+db.getCollection("DemoCollection").drop();
+db.createCollection("DemoCollection");
+
+// ----------------------------
+// Documents of DemoCollection
+// ----------------------------
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06ef9378c3c1dae0632c4"),
+    title: "DuBuquefurt",
+    description: "Laborum aut mollitia facilis libero sint assumenda. Explicabo et et est vel voluptatem et. Vel mollitia distinctio corporis sed dolore et qui. Nesciunt aut maiores nostrum et natus in facere.",
+    likes: NumberInt("42238"),
+    url: "http://marquardt.com/quisquam-exercitationem-in-dolor-voluptates-eius",
+    by: "Elinore McDermott"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f20378c3c1dae0632c5"),
+    title: "Hellerstad",
+    description: "Et quia rem magni laborum voluptas et. Deserunt neque quis qui sit odit qui. Pariatur rerum eaque autem quas.",
+    likes: NumberInt("541958"),
+    url: "http://www.streich.info/amet-explicabo-at-reprehenderit-saepe.html",
+    by: "Miss Estel Rutherford IV"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f2f378c3c1dae0632c6"),
+    title: "North Zena",
+    description: "Voluptates porro sit aut et nulla veniam voluptates. Fugiat molestias laborum atque inventore pariatur vero unde. Facilis quibusdam repellendus velit labore nemo aliquam aliquid. Autem nostrum aut est deserunt. Voluptas sed autem assumenda laboriosam perspiciatis quae repellendus.",
+    likes: NumberInt("47979"),
+    url: "https://www.flatley.com/magnam-omnis-corrupti-tempora-voluptatem-et-et-nemo",
+    by: "Baylee Veum"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3c378c3c1dae0632c7"),
+    title: "Muellerside",
+    description: "Neque maiores voluptas est vel et facilis magni deserunt. Eaque sint ut aut consequuntur. Neque natus incidunt facere expedita dolor. Aut omnis commodi repellendus.",
+    likes: NumberInt("810228"),
+    url: "https://www.barrows.net/facilis-at-quo-voluptas-soluta-iusto",
+    by: "Weldon Mills"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3d378c3c1dae0632c8"),
+    title: "Beckerport",
+    description: "Ipsa nam qui nam ipsa vero nisi. Quia vel eum facilis et qui. Quos ut alias laborum quia est. Iste rerum amet autem optio quia.",
+    likes: NumberInt("954407"),
+    url: "http://www.sporer.com/est-nesciunt-quos-tempora",
+    by: "Treva Glover"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3d378c3c1dae0632c9"),
+    title: "Port Pedro",
+    description: "Est beatae sed voluptas quis natus. Error ut omnis sint sunt. Harum dolorum tempore sed et commodi nihil quos. Aliquam autem ipsa molestiae eaque atque ducimus dolore. Dolorum sunt tempora saepe eius nihil adipisci.",
+    likes: NumberInt("492817"),
+    url: "http://www.rolfson.com/",
+    by: "Melany Mante"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3e378c3c1dae0632ca"),
+    title: "Janicemouth",
+    description: "Rem rerum qui ullam dolorem. Occaecati et qui non et aliquid id laudantium. Quas quam voluptatem consequatur quis vero. Sunt deserunt dolorem magni atque magni repellat.",
+    likes: NumberInt("713266"),
+    url: "http://www.blanda.biz/laboriosam-ducimus-eos-explicabo-nobis-velit-rem-quia.html",
+    by: "Julian Purdy"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3e378c3c1dae0632cb"),
+    title: "Jovannyview",
+    description: "Vero quos perspiciatis temporibus et. Et soluta laborum temporibus et aperiam tenetur nobis. Et excepturi minus dolorem fuga.",
+    likes: NumberInt("228181"),
+    url: "http://www.pouros.com/temporibus-sed-natus-incidunt-accusamus-ullam-culpa-dolorum.html",
+    by: "Abdul Stamm"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3f378c3c1dae0632cc"),
+    title: "Arthurmouth",
+    description: "Dolorem optio repudiandae impedit omnis eaque saepe. Quia dolorem sit voluptates quas similique quidem iure natus. Molestiae voluptas ut quia aut vitae. Et enim omnis pariatur est rerum autem in.",
+    likes: NumberInt("769610"),
+    url: "http://www.quigley.com/ex-hic-sunt-aspernatur-sit",
+    by: "Flavio Abshire"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3f378c3c1dae0632cd"),
+    title: "Jalonfurt",
+    description: "Aut veniam nobis dignissimos et natus aliquam eveniet aspernatur. Voluptas non in facilis earum magnam doloremque maxime ut. Perspiciatis similique et et magni adipisci eum. Velit est facilis at laboriosam.",
+    likes: NumberInt("194322"),
+    url: "https://bayer.com/velit-accusantium-voluptas-maxime-voluptatem.html",
+    by: "Larry Oberbrunner"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f3f378c3c1dae0632ce"),
+    title: "Lake Ivah",
+    description: "Vitae assumenda consequuntur inventore est dolorem. Eligendi est ea rerum omnis vel autem. Distinctio et sint dolores ipsa sunt ea.",
+    likes: NumberInt("888372"),
+    url: "http://gibson.com/illo-sequi-dignissimos-iusto-et-dolorem-repellendus.html",
+    by: "Shayna Jakubowski Sr."
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d06f40378c3c1dae0632cf"),
+    title: "North Maribel",
+    description: "Aliquam et inventore repellat non labore explicabo. Ipsam harum accusantium quam velit rem aliquam.",
+    likes: NumberInt("971226"),
+    url: "http://www.wolf.org/natus-maxime-natus-quaerat-quae.html",
+    by: "Dr. Maverick Hagenes"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d07029378c3c1dae0632d0"),
+    title: "South Andreston",
+    description: "Sunt totam est qui perspiciatis. Est molestias provident facere aut.",
+    likes: NumberInt("884183"),
+    url: "http://koch.com/id-corrupti-amet-non-nemo",
+    by: "Dr. Alejandrin Goldner"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d07e9a378c3c1dae0632d1"),
+    title: "Kshlerinview",
+    description: "Id quod quidem cum incidunt. Distinctio atque porro amet ratione incidunt quisquam voluptatem asperiores. Minus et consectetur ducimus rerum. Iste aliquam eum ipsam in.",
+    likes: NumberInt("130956"),
+    url: "http://www.terry.com/sed-consectetur-eveniet-id-officia-quia.html",
+    by: "Vergie Gorczany"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d07f56378c3c1dae0632d2"),
+    title: "Lake Ila",
+    description: "Illum quis quaerat saepe sunt. Non exercitationem est ut in natus. Nobis voluptatibus neque ut dignissimos a ipsum.",
+    likes: NumberInt("530764"),
+    url: "http://feil.org/",
+    by: "Myrtis Thompson"
+} ]);
+db.getCollection("DemoCollection").insert([ {
+    _id: ObjectId("65d08194378c3c1dae0632d3"),
+    title: "Kirlinfurt",
+    description: "Rerum eum aspernatur vel enim. Dolore fugiat velit deleniti reprehenderit molestias sit est unde. Tenetur minima corporis delectus exercitationem sed officia quia. Libero exercitationem qui veritatis rerum ut.",
+    likes: NumberInt("14448"),
+    url: "http://www.hirthe.com/et-ab-quae-molestiae-laborum.html",
+    by: "Hosea Marvin"
+} ]);
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ © Copyright 2024 tuyck.com
+ * by Alireza Nazari
+ * email alireza.ginbox@gmail.com
+ * version 1
+ * last update 17-02-2024
+*/
+
+require './vendor/autoload.php';
+
+// connect to mongodb you can replace the root user and toor password with your credential
+$m = new MongoDB\Client('mongodb://root:toor@127.0.0.1:27017', [
+    'directConnection' => true,
+    'serverSelectionTimeoutMS' => 2000
+]);
+if ($m) dump("Connection to database successfully");
+
+// select a database Demo (you need create required database before using it in here )
+$db = $m->Demo;
+if ($db) dump( "Database Demo selected");
+
+// select a collection from our Demo database (you may change the DemoCollection your collection)
+$collection = $db->DemoCollection;
+if ($collection) dump("Collection selected succsessfully");
+
+// make fake random data
+$document = array(
+    "title" => Faker\Factory::create()->city,
+    "description" => Faker\Factory::create()->paragraph,
+    "likes" => Faker\Factory::create()->numberBetween(1,1000000),
+    "url" => Faker\Factory::create()->url,
+    "by" => Faker\Factory::create()->name
+);
+
+// inset the fake data to the selected collection
+$insertOneResult = $collection->insertOne($document);
+if ($insertOneResult){
+    dump($insertOneResult->getInsertedId() ." Document inserted successfully");
+}
+
+// search the selected collection for data for example "db.DemoCollection.find() or in sql select * form DemoCollection"
+$cursor = $collection->find();
+// iterate cursor to display title of documents
+foreach ($cursor as $document) {
+    dump($document->title);
+}
